dd11c0baa442ef8c0762320cdbaf5eb1c5428aa8
DarkMolly <57105640+BumbumKill@users.noreply.github.com>
2020-02-09 12:47:44 +0700

    fix

--- src/rr/Main.php
+++ src/rr/Main.php
@@ -113,9 +113,9 @@ public function rename(Player $sender){
           $rename = $this->getConfig()->get("price-rename");
           if($mymoney >= $rename){
 	      $economy->reduceMoney($sender, $rename);
-                $item->setCustomName($this->color(), $data[1]);
+                $item->setCustomName(T::colorize($data[1]));
                 $sender->getInventory()->setItemInHand($item);
-                $sender->sendMessage(T::GREEN . "successfully changed item name to §e$data[1]");
+                $sender->sendMessage(T::GREEN . "successfully changed item name to §r$data[1]");
                 }else{
              $sender->sendMessage(T::RED . "You don't have enough money!");
              }
@@ -128,28 +128,5 @@ public function rename(Player $sender){
 	  $f->addLabel("§aRename cost: §e$rename\n§bYour money: $mymoney");
           $f->addInput(T::RED . "Rename Item:", "HardCore");
 	  $f->sendToPlayer($sender);
-	     }
-
-    private function color($string)
-    {
-        $string = str_replace("{COLOR_BLACK}", "&0", $string);
-        $string = str_replace("{COLOR_DARK_BLUE}", "&1", $string);
-        $string = str_replace("{COLOR_DARK_GREEN}", "&2", $string);
-        $string = str_replace("{COLOR_DARK_AQUA}", "&3", $string);
-        $string = str_replace("{COLOR_DARK_RED}", "&4", $string);
-        $string = str_replace("{COLOR_DARK_PURPLE}", "&5", $string);
-        $string = str_replace("{COLOR_GOLD}", "&6", $string);
-        $string = str_replace("{COLOR_GRAY}", "&7", $string);
-        $string = str_replace("{COLOR_DARK_GRAY}", "&8", $string);
-        $string = str_replace("{COLOR_BLUE}", "&9", $string);
-        $string = str_replace("{COLOR_GREEN}", "&a", $string);
-        $string = str_replace("{COLOR_AQUA}", "&b", $string);
-        $string = str_replace("{COLOR_RED}", "&c", $string);
-        $string = str_replace("{COLOR_LIGHT_PURPLE}", "&d", $string);
-        $string = str_replace("{COLOR_YELLOW}", "&e", $string);
-        $string = str_replace("{COLOR_WHITE}", "&f", $string);
-        $string = str_replace("{FORMAT_OBFUSCATED}", "&k", $string);
-        $string = str_replace("{FORMAT_BOLD}", "&l", $string);
-        return $string;
-    }
+   }
 }
